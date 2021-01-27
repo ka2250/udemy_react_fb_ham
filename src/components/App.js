272ce4ecb62b024ea1d10ff6265ff5d886d6c6ec
ka2250 <katok2250@gmail.com>
2021-01-27 22:51:52 +0900

    create config

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
+import config from "../config.json";
 import SignIn from "./SignIn";
 import Main from "./Main";
 
 const App = () => {
 	const [name, setName] = useState("");
 
-	if (name === "") {
+	if (config.signInEnabled && name === "") {
 		return <SignIn setName={setName} />;
 	} else {
 		return <Main name={name} />;
