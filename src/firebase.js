b5b371156cfb9e315c07d88db86614c5282fc6ea
ka2250 <katok2250@gmail.com>
2021-01-28 10:40:52 +0900

    fix: projectIdのタイポを修正

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ const {
 	REACT_APP_FIREBASE_API_KEY,
 	REACT_APP_FIREBASE_AUTH_DOMAIN,
 	REACT_APP_FIREBASE_DATABASE_URL,
-	REACT_APP_FIREBASE_PROJECT_Id,
+	REACT_APP_FIREBASE_PROJECT_ID,
 	REACT_APP_FIREBASE_STORAGE_BUCKET,
 	REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
 	REACT_APP_FIREBASE_APP_ID,
@@ -14,16 +14,16 @@ const firebaseConfig = {
 	apiKey: REACT_APP_FIREBASE_API_KEY,
 	authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
 	databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
-	projectId: REACT_APP_FIREBASE_PROJECT_Id,
+	projectId: REACT_APP_FIREBASE_PROJECT_ID,
 	storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
 	messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
 	appId: REACT_APP_FIREBASE_APP_ID,
 };
-firebase.initializeApp(firebaseConfig);
 
+firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
-const messageRef = database.ref("messages");
+export const messagesRef = database.ref("messages");
 
 export const pushMessage = ({ name, text }) => {
-	messageRef.push({ name, text });
+	messagesRef.push({ name, text });
 };
